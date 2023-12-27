41ac14732f94e8f14d76f6922de739755f6f2976
Charlie <gamerboy030607@gmail.com>
2023-12-28 04:20:50 +1100

    finish drafg

--- main.py
+++ main.py
@@ -17,47 +17,48 @@ from PySide6.QtSvg import *
 from PySide6.Qt3DCore import *
 from PySide6.Qt3DRender import *
 from PySide6.Qt3DExtras import *
+
 # list of CSharp controls
 items = [
-    ("Button", "<Button />"),
-    ("Label", "<Label />"),
-    ("Textbox", "<Textbox />"),
-    ("Checkbox", "<Checkbox />"),
-    ("Radio Button", "<RadioButton />"),
-    ("Listbox", "<Listbox />"),
-    ("Combobox", "<Combobox />"),
-    ("Image", "<Image />"),
-    ("Video", "<Video />"),
-    ("Audio", "<Audio />"),
-    ("Webview", "<Webview />"),
-    ("Canvas", "<Canvas />"),
-    ("Chart", "<Chart />"),
-    ("Table", "<Table />"),
-    ("Grid", "<Grid />"),
-    ("Panel", "<Panel />"),
-    ("Groupbox", "<Groupbox />"),
-    ("Tab", "<Tab />"),
-    ("Accordion", "<Accordion />"),
-    ("Progressbar", "<Progressbar />"),
-    ("Slider", "<Slider />"),
-    ("Spinner", "<Spinner />"),
-    ("Calendar", "<Calendar />"),
-    ("Clock", "<Clock />"),
-    ("Map", "<Map />"),
-    ("QR Code", "<QRCode />"),
-    ("Barcode", "<Barcode />"),
-    ("Color Picker", "<ColorPicker />"),
-    ("Date Picker", "<DatePicker />"),
-    ("Time Picker", "<TimePicker />"),
-    ("File Picker", "<FilePicker />"),
-    ("Treeview", "<Treeview />"),
-    ("Menu", "<Menu />"),
-    ("Toolbar", "<Toolbar />"),
-    ("Statusbar", "<Statusbar />"),
-    ("Dialog", "<Dialog />"),
-    ("Tooltip", "<Tooltip />"),
-    ("Notification", "<Notification />"),
-    ("Toast", "<Toast />"),
+    ("Button", "<Button "),
+    ("Label", "<Label "),
+    ("Textbox", "<Textbox "),
+    ("Checkbox", "<Checkbox "),
+    ("Radio Button", "<RadioButton "),
+    ("Listbox", "<Listbox "),
+    ("Combobox", "<Combobox "),
+    ("Image", "<Image "),
+    ("Video", "<Video "),
+    ("Audio", "<Audio "),
+    ("Webview", "<Webview "),
+    ("Canvas", "<Canvas "),
+    ("Chart", "<Chart "),
+    ("Table", "<Table "),
+    ("Grid", "<Grid "),
+    ("Panel", "<Panel "),
+    ("Groupbox", "<Groupbox "),
+    ("Tab", "<Tab "),
+    ("Accordion", "<Accordion "),
+    ("Progressbar", "<Progressbar "),
+    ("Slider", "<Slider "),
+    ("Spinner", "<Spinner "),
+    ("Calendar", "<Calendar "),
+    ("Clock", "<Clock "),
+    ("Map", "<Map "),
+    ("QR Code", "<QRCode "),
+    ("Barcode", "<Barcode "),
+    ("Color Picker", "<ColorPicker "),
+    ("Date Picker", "<DatePicker "),
+    ("Time Picker", "<TimePicker "),
+    ("File Picker", "<FilePicker "),
+    ("Treeview", "<Treeview "),
+    ("Menu", "<Menu "),
+    ("Toolbar", "<Toolbar "),
+    ("Statusbar", "<Statusbar "),
+    ("Dialog", "<Dialog "),
+    ("Tooltip", "<Tooltip "),
+    ("Notification", "<Notification "),
+    ("Toast", "<Toast "),
     
 ]
 
@@ -74,6 +75,9 @@ class CustomWidget(QGraphicsItem):
             self.text = ""  # Initialize text
             self.resizeMode = None  # Initialize resizeMode
 
+        
+            
+        
         def boundingRect(self):
             return self.rect
 
@@ -108,7 +112,7 @@ class CustomWidget(QGraphicsItem):
                     self.resizeMode = "center"
                 else:
                     self.setCursor(Qt.ClosedHandCursor)
-                    self.resizeMode = None
+                    self.resizeMode = "move"  # Set resizeMode to "move" instead of None
             super().mousePressEvent(event)
 
         def mouseMoveEvent(self, event):
@@ -117,8 +121,9 @@ class CustomWidget(QGraphicsItem):
             elif self.resizeMode == "center":
                 delta = event.pos() - event.lastPos()
                 self.rect.translate(delta)
-            else:
+            elif self.resizeMode == "move":  # Add this condition
                 super().mouseMoveEvent(event)
+            self.prepareGeometryChange()  # Notify the view that the geometry has changed
 
         def mouseReleaseEvent(self, event):
             self.setCursor(Qt.ArrowCursor)
@@ -144,11 +149,29 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("WYSIWYG GUI Editor")
-
+        
         self.resize(800, 600)
         self.drawcanvas()
+    
+    def contextMenuEvent(self, event):
+        menu = QMenu(self)
+        menu.addAction("Delete")
+        menu.triggered.connect(self.Delete)
+        # connect the menu to the actions 
         
-        
+        menu.exec(event.globalPos())
+    
+    def Delete(self):
+        # delete the selected item
+        for item in self.canvas.selectedItems():
+            self.canvas.removeItem(item)
+            
+    def Cut(self):
+        # cut the selected item
+        for item in self.canvas.selectedItems():
+            self.canvas.cutItem(item)
+            
+    
     def drawcanvas(self):
         # buttons and lists that can be dragged and dropped
         self.sidebar = QDockWidget("Sidebar", self)
@@ -184,23 +207,34 @@ class MainWindow(QMainWindow):
         
         
         # setup menu options
-        # Add menu items to an already set addAction()
         fileMenu = self.menuBar.addMenu("File")
-        fileMenu.addAction("New")
-        fileMenu.addAction("Open")
-        fileMenu.addAction("Save")
-        Exportmenu = fileMenu.addMenu("Export")``
-        Exportmenu.addAction("Export to C#")
+
+        # Create actions
+        newAction = QAction("New", self)
+        openAction = QAction("Open", self)
+        saveAction = QAction("Save", self)
+        exportAction = QAction("Export to C#", self)
+
+        # Add actions to the menu
+        fileMenu.addAction(newAction)
+        fileMenu.addAction(openAction)
+        fileMenu.addAction(saveAction)
+
+        # Create Export submenu
+        exportMenu = QMenu("Export", self)
+        exportMenu.addAction(exportAction)
+        fileMenu.addMenu(exportMenu)
+
+        # Bind each action to its respective function
         
-        # setup bindings
-        self.sidebar.widget().itemClicked.connect(self.addwidget)
-        self.view.dropEvent = self.dropEvent
-        self.view.dragEnterEvent = self.dragEnterEvent
-        self.view.dragMoveEvent = self.dragMoveEvent
+        saveAction.triggered.connect(self.save)
+        openAction.triggered.connect(self.open)
+        exportAction.triggered.connect(self.exporttoxaml)
+
         
-        # bind the menu buttons
         
-        Exportmenu.triggered.connect(self.exporttoxaml)
+        
+        
         
         # self.view.mousePressEvent = self.mousePressEvent
         # self.view.mouseMoveEvent = self.mouseMoveEvent
@@ -216,12 +250,61 @@ class MainWindow(QMainWindow):
         # set widget size
         widget.rect = QRectF(0, 0, 100, 100)
         widget.update()
+
+    def save(self):
+        # save the canvas to a file
+        with open("output.json", "a") as file:
+            data = []
+            for item in self.canvas.items():
+                if isinstance(item, CustomWidget):
+                    # get the name of the item and the location
+                    item_name = item.text
+                    item_rect = {
+                        "x": item.rect.x(),
+                        "y": item.rect.y(),
+                        "width": item.rect.width(),
+                        "height": item.rect.height()
+                    }
+                    data.append({
+                        "name": item_name,
+                        "rect": item_rect
+                    })
+            json.dump(data, file, indent=4)
+            file.write("\n")  # Append a new line after each item
+    
+    def open(self):
+        # open a window to select a file
+        filename = QFileDialog.getOpenFileName(self, "Open File", "", "JSON Files (*.json)")
+        if filename[0]:
+            with open(filename[0], "r") as file:
+                data = json.load(file)
+                for item in data:
+                    widget = CustomWidget()
+                    widget.setPos(item["rect"]["x"], item["rect"]["y"])
+                    self.canvas.addItem(widget)
+                    widget.setText(item["name"])
+                    widget.rect = QRectF(item["rect"]["x"], item["rect"]["y"], item["rect"]["width"], item["rect"]["height"])
+                    widget.update()
+    
+   
         
     def exporttoxaml(self):
         # export to xaml
-        
+        xaml_content = ""
+ 
         for item in self.canvas.items():
-            print(item)
+            if isinstance(item, CustomWidget):  # Check if item is an instance of CustomWidget
+                # get the name of the item and the location
+                item_name = item.text  # Assuming the item has a text property
+                item_location = item.pos()
+                item_size = item.rect
+
+                # Append the XAML representation of the item to the content
+                xaml_content += f'{item_name} HorizontalAlignment="Left" VerticalAlignment="Top" Margin="{-item_location.x()}, {-item_location.y()}, 0,0" Height="{item_size.height()}" Width="{item_size.width()}"  />\n'
+
+        # Save the XAML content to a file
+        with open("output.xaml", "w") as file:
+            file.write(xaml_content)
         
         
         
